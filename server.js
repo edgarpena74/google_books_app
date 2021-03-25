50facea469d6b3d80207631bb2df0d9250e3b633
Edgar Pena <edgarignaciopena@gmail,com>
2021-03-24 20:37:15 -0700

    updated .env

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-//test
 //api routes starts with "/api"
 app.use("/api", require("./routes/apiRoutes"));
 
